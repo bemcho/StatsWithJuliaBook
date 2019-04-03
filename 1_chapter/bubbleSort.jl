6b9f0f88a47e855dc01050700291974e3dfaf6fa
Emil Tomov <emil.tomov@rewe-digital.com>
2019-04-03 15:16:14 +0300

    syncing progress

--- 1_chapter/bubbleSort.jl
+++ 1_chapter/bubbleSort.jl
@@ -1,7 +1,7 @@
-function bubbleSort!(a)
+function simdBubbleSort!(a)
     n = length(a)
-    for i in 1:n-1
-        for j in 1:n-i
+ @simd for i in 1:n-1
+    @simd    for j in 1:n-i
             if a[j] > a[j+1]
                 a[j], a[j+1] = a[j+1], a[j]
             end
@@ -10,5 +10,22 @@ function bubbleSort!(a)
     return a
 end
 
+function bubbleSort!(a)
+    n = length(a)
+ @simd for i in 1:n-1
+    @simd    for j in 1:n-i
+            if a[j] > a[j+1]
+                a[j], a[j+1] = a[j+1], a[j]
+            end
+        end
+    end
+    return a
+end
+
+
 data = [65, 51, 32, 12, 23, 84, 68, 1]
-bubbleSort!(data)
+const randCount = 10^7
+@time bubbleSort!(rand(randCount))
+@time simdBubbleSort!(rand(randCount))
+@time sort(rand(randCount))
+@time sort!(rand(randCount))
